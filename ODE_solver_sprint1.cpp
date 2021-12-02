9629e29b73aab6714b3f4195a52ed49bc4d17037
Omeralp Sahin <ge47nid@mytum.de>
2021-12-03 00:07:23 +0100

    commit3-function_trying

--- ODE_solver_sprint1.cpp
+++ ODE_solver_sprint1.cpp
@@ -1,2 +1,12 @@
-#include iostream.h
+#include <iostream>
+#include <cmath>
+
+int function(int a, int b, int x, int y){
+	cout << "Please enter the coefficient of x: "; // f(x,y), where dy/dx=f(x,y), x(x0)=yo
+	cin >> a;
+	cout << "Please enter the coefficient of y: ";
+	cin >> b;
+	cout << "Your function is : " << a << "x + " << b << "y";
+	return ax+by
+}
 int main()
