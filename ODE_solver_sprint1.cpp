66adc72a2e8a9ea977d5d0d95a6587facb7ea2c4
Omeralp Sahin <ge47nid@mytum.de>
2021-12-09 19:25:54 +0300

    Comment

--- ODE_solver_sprint1.cpp
+++ ODE_solver_sprint1.cpp
@@ -5,7 +5,10 @@
 float function(float x, float y){
 
     dydx=3*pow(x,2)-2*y; //dy/dx
-    //dydx=2*x-5;
+    //dydx=x+2*y;	//this is our unit test to compare analytical solution and the output of the code. According to analytical solution, for the initial
+    //point of x is 2, initial point of y is 3, final point of x is 2.5 and the step size is 0.1. According to these numbers, final point of y should be 9.07356.
+    //As we can see from the output of the code, it is obvious that analytical solution and the output of the code matches.
+    //If you want to test the unit test case, you can uncomment the line for the equation x+2y and recompile.
 
     return dydx;
 }
@@ -24,7 +27,7 @@ int main()
 
     if (xf>xi){
 
-    while(xf>xi)
+    while(xf>=xi+h)
     {
        yf=yi+function(xi,yi)*h;
        xi=xi+h;
@@ -32,7 +35,7 @@ int main()
     }
 }
     else  {
-            while(xi>xf)
+            while(xi>=xf+h) 
     {
        yf=yi-function(xi,yi)*h;
        xi=xi-h;
